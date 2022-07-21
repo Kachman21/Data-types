b1d0956029fbc0668fe186f5f388386f539c1937
Kachman21 <Kachman21onyedikachiokafor21@gmail.com>
2022-07-21 14:09:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("hello")
+/*let m="orange"
+console.log(m)
+let name= "kachi "+ "Hub"
+console.log(name)
+let x= 20
+let y=4
+let multiply= x*y
+console.log(multiply)
+let subtract= x-y
+console.log(subtract)
+let divide= x/y
+console.log(divide)
+let add= x+y
+console.log(add)
+let equals= x==y
+console.log(equals)
+let notequals= x!=y
+console.log(notequals)
+let bad= x>y
+conole.log(bad)
+let good= x<y
+console.log(good)*/
